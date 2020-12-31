c75666b17dc367858bfbbca20bab79fbac07b492
Adam <adam.holo@mail.ru>
2020-12-31 09:21:46 +0400

    I made up the login form. I'm working on media queries.

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,11 @@
 import $ from 'jquery';
 import '../scss/main.scss';
 import '../index.pug';
-
+import '../fonts/Montserrat-Black.woff2';
+import '../fonts/Montserrat-Bold.woff2';
+import '../img/svg/header-logo.svg';
+import '../img/svg/instagram.svg';
+import '../img/svg/twitter.svg';
+import '../img/svg/facebook.svg';
+import '../img/bg.jpg';
+import './ibg.js'
